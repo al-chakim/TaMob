50db0f24757d969658f7641eee7705756aa1b3ad
Muchammad Amru Al-Chakimi <amrualchakim@gmail.com>
2022-06-03 19:36:21 +0700

    mulai

---
+++ golodors/lib/color.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color satu = Color(0xff79C4FA);
+Color dua = Color(0xff035692);
---
+++ golodors/lib/mulai.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:golodors/color.dart';
+
+class mulai extends StatefulWidget {
+  const mulai({ Key? key }) : super(key: key);
+
+  @override
+  State<mulai> createState() => _mulaiState();
+}
+
+class _mulaiState extends State<mulai> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: satu,
+    );
+  }
+}
