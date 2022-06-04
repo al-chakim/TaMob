c55fe19174e5984989c91e5f3bb17ff91a3c3a7a
Muchammad Amru Al-Chakimi <amrualchakim@gmail.com>
2022-06-04 14:55:33 +0700

    masuk

--- golodors/lib/color.dart
+++ golodors/lib/color.dart
@@ -2,3 +2,10 @@ import 'package:flutter/material.dart';
 
 Color satu = Color(0xff79C4FA);
 Color dua = Color(0xff035692);
+Color tiga = Color(0xffFFFFFF);
+Color empat = Color(0xff000000);
+
+TextStyle huruf1 = TextStyle(
+  color: tiga,
+  fontWeight: FontWeight.w700
+);
--- golodors/lib/mulai.dart
+++ golodors/lib/mulai.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:golodors/color.dart';
+import 'package:golodors/masuk.dart';
 
 class mulai extends StatefulWidget {
-  const mulai({ Key? key }) : super(key: key);
+  const mulai({Key? key}) : super(key: key);
 
   @override
   State<mulai> createState() => _mulaiState();
@@ -13,6 +14,84 @@ class _mulaiState extends State<mulai> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: satu,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 120,
+            ),
+            Image.asset(
+              'assets/p1.png',
+              width: 170,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'GOLODORS',
+              style: huruf1.copyWith(fontSize: 25),
+            ),
+            SizedBox(
+              height: 150,
+            ),
+            Container(
+              height: 50,
+              width: 330,
+              //padding: EdgeInsets.only(left: 20, top: 6),
+              margin: EdgeInsets.only(right: 20, left: 20, top: 10, bottom: 10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(50),
+                      topLeft: Radius.circular(8),
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(8)),
+                  color: dua),
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => login()));
+                  },
+                  child: Center(
+                    child: Text(
+                      'Sign In',
+                      style: huruf1.copyWith(fontSize: 17.5),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: 330,
+              //padding: EdgeInsets.only(left: 20, top: 6),
+              margin: EdgeInsets.only(right: 20, left: 20, top: 10, bottom: 10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(8),
+                      topLeft: Radius.circular(50),
+                      bottomLeft: Radius.circular(8),
+                      bottomRight: Radius.circular(50)),
+                  color: tiga),
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () {},
+                  child: Center(
+                    child: Text(
+                      'Sign Up',
+                      style: huruf1.copyWith(fontSize: 17.5, color: empat),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 30,),
+            
+          ],
+        ),
+      ),
     );
   }
 }
