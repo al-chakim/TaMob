ffa3ec7098eb36c16bda0ee5be507abbbdc216d7
Muchammad Amru Al-Chakimi <amrualchakim@gmail.com>
2022-06-04 17:17:09 +0700

    icon

--- golodors/lib/masuk.dart
+++ golodors/lib/masuk.dart
@@ -14,8 +14,8 @@ class _loginState extends State<login> {
     return Scaffold(
       backgroundColor: satu,
       body: ListView(
-        children: 
-          [Center(
+        children: [
+          Center(
             child: Column(
               children: [
                 SizedBox(
@@ -36,54 +36,143 @@ class _loginState extends State<login> {
                   height: 50,
                 ),
                 Container(
-                  height: 40,
-                  width: 150,
+                  height: 45,
+                  width: 330,
                   //padding: EdgeInsets.only(left: 20, top: 6),
-                  margin: EdgeInsets.only(right: 20, left: 20, top: 10, bottom: 10),
+                  margin:
+                      EdgeInsets.only(right: 20, left: 20, top: 10, bottom: 10),
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10), color: Colors.transparent),
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white70),
                   child: Material(
                     color: Colors.transparent,
                     child: InkWell(
                       onTap: () {},
                       child: Center(
                         child: TextField(
-                        autofocus: false,
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                          labelText: 'Username',
+                          //autofocus: false,
+                          decoration: InputDecoration(
+                            contentPadding:
+                                EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            hintText: 'Username',
+                          ),
                         ),
-                      ),
                       ),
                     ),
                   ),
                 ),
                 //SizedBox(height: 10,),
                 Container(
-                  height: 40,
-                  width: 150,
+                  height: 45,
+                  width: 330,
                   //padding: EdgeInsets.only(left: 20, top: 6),
-                  margin: EdgeInsets.only(right: 20, left: 20, top: 10, bottom: 10),
+                  margin:
+                      EdgeInsets.only(right: 20, left: 20, top: 10, bottom: 10),
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10), color: Colors.transparent),
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white70),
                   child: Material(
                     color: Colors.transparent,
                     child: InkWell(
                       onTap: () {},
                       child: Center(
                         child: TextField(
-                        autofocus: false,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                          labelText: 'Password',
+                          autofocus: false,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            contentPadding:
+                                EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            hintText: 'Password',
+                          ),
                         ),
-                      ),
                       ),
                     ),
                   ),
+                ),
+                SizedBox(height: 10,),
+                Text('OR', style: huruf1.copyWith(fontSize: 18),),
+                SizedBox(height: 10,),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 12),
+                      child: Container(
+                          height: 40,
+                          width: 160,
+                          //padding: EdgeInsets.only(left: 20, top: 6),
+                          margin: EdgeInsets.only(
+                            //right: 20, 
+                            left: 20, 
+                            top: 10, 
+                            bottom: 10
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Color(0xff3b5998)
+                          ),
+                          child: Material(
+                            color: Colors.transparent,
+                            child: InkWell(
+                              onTap: () {},
+                              child: Center(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.facebook_sharp, color: tiga,),
+                                    SizedBox(width: 5,),
+                                    Text(
+                                      'facebook',
+                                      style: huruf1,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 6),
+                      child: Container(
+                          height: 40,
+                          width: 160,
+                          //padding: EdgeInsets.only(left: 20, top: 6),
+                          margin: EdgeInsets.only(
+                            right: 20, 
+                            //left: 20, 
+                            top: 10, 
+                            bottom: 10
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: tiga
+                          ),
+                          child: Material(
+                            color: Colors.transparent,
+                            child: InkWell(
+                              onTap: () {},
+                              child: Center(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    //Icon(Icons.)
+                                    Text(
+                                      'Google',
+                                      style: huruf2,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                    ),
+                  ],
                 ),
               ],
             ),
